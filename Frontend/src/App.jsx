420be1cde4926ff82ea30257344208e73f5204b9
DanielRivni <drivni123@gmail.com>
2024-01-25 18:11:10 +0200

    Front init

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,21 @@
+import SignUpPage from './pages/SignUpPage.jsx'
 import HomePage from './pages/HomePage.jsx'
+import RequestsPage from './pages/RequestsPage.jsx'
+// import OpenRequestPage from './pages/OpenRequestPage.jsx'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
-
   return (
-    <>
-      <HomePage/>
-    </>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/SignUpPage" element={<SignUpPage />} />
+          <Route path="/RequestsPage" element={<RequestsPage />} /> 
+          {/* <Route path="/OpenRequestPage" element={<OpenRequestPage />} /> */}
+        </Routes>
+      </div>
+    </Router>
   )
 }
 

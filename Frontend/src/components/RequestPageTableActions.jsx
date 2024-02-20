e322b8ecb6bbd854bfbdbda7dd03230169acd3b3
DanielRivni <drivni123@gmail.com>
2024-02-20 17:04:08 +0200

    Added delete task

--- Frontend/src/components/RequestPageTableActions.jsx
+++ Frontend/src/components/RequestPageTableActions.jsx
@@ -8,23 +8,45 @@ import {
   ListItemIcon,
   ListItemText,
   Button,
+  Snackbar,
 } from "@mui/material";
 import SettingsIcon from "@mui/icons-material/Settings";
 import CancelIcon from "@mui/icons-material/Cancel";
-import VolLocationDialog from "./VolLocationDialog.jsx";
-import "../styles/RequestsPage.css";
+import VolLocationDialog from './VolLocationDialog.jsx'
+import '../styles/RequestsPage.css';
+import { deleteFirestoreDocument } from "../middleware/firestore/index"; // Import the function to delete a document
 
 const TableActionsDialog = ({
   uid,
   handleDeleteRequest,
   isActiveTask,
-  status, // Pass status prop from parent component
-  volLocation,
+  status,
+  volLocation
 }) => {
   const [tableActionsDialogState, setTableActionsDialogState] = useState(false);
+  const [snackbarSuccessOpen, setSnackbarSuccessOpen] = useState(false);
+  const [snackbarErrorOpen, setSnackbarErrorOpen] = useState(false);
 
   const handleTableActionsDialogClose = () => {
     setTableActionsDialogState(false);
+  }
+
+  const handleDeleteButtonClick = async () => {
+    if (status === "נסגר") {
+      await deleteFirestoreDocument("requests", uid);
+      setSnackbarSuccessOpen(true);
+    } else {
+      console.log("Cannot delete request because status is not over");
+      setSnackbarErrorOpen(true);
+    }
+  };
+
+  const handleSnackbarSuccessClose = () => {
+    setSnackbarSuccessOpen(false);
+  };
+
+  const handleSnackbarErrorClose = () => {
+    setSnackbarErrorOpen(false);
   };
 
   return (
@@ -39,32 +61,31 @@ const TableActionsDialog = ({
         >
           <SettingsIcon />
         </Button>
-        <Dialog
-          open={tableActionsDialogState}
-          onClose={handleTableActionsDialogClose}
-        >
+        <Dialog open={tableActionsDialogState} onClose={handleTableActionsDialogClose}>
           <DialogTitle>פעולות נוספות</DialogTitle>
           <DialogContent>
             <List className="openTask-task-list">
-              <VolLocationDialog volLocation={volLocation} status={status} />{" "}
-              {/* Pass status prop */}
+              {/* Conditionally render the Delete Request button */}
+              {status === "נסגר" && (
+                <ListItemButton
+                  onClick={handleDeleteButtonClick}
+                  sx={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', gap: '2em'}}
+                >
+                  <ListItemText primary={"מחיקת בקשה"} />
+                </ListItemButton>
+              )}
+              <VolLocationDialog volLocation={volLocation}/>
               <ListItemButton
                 onClick={() => handleDeleteRequest(uid)}
                 disabled={!isActiveTask(status)}
-                sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                  gap: "2em",
-                }}
+                sx={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', gap: '2em'}}
                 style={{
                   backgroundColor: isActiveTask(status) ? "#FF3333  " : "",
                   color: isActiveTask(status) ? "white" : "primary",
                 }}
               >
                 <ListItemText primary={"ביטול סיוע"} />
-                <ListItemIcon sx={{ minWidth: "0", color: "white" }}>
+                <ListItemIcon sx={{minWidth: '0', color: 'white'}}>
                   <CancelIcon />
                 </ListItemIcon>
               </ListItemButton>
@@ -72,6 +93,18 @@ const TableActionsDialog = ({
           </DialogContent>
         </Dialog>
       </div>
+      <Snackbar
+        open={snackbarSuccessOpen}
+        autoHideDuration={6000}
+        onClose={handleSnackbarSuccessClose}
+        message="הבקשה נמחקה בהצלחה"
+      />
+      <Snackbar
+        open={snackbarErrorOpen}
+        autoHideDuration={6000}
+        onClose={handleSnackbarErrorClose}
+        message="לא ניתן למחוק את הבקשה מכיוון שהסטטוס אינו נסגר"
+      />      
     </>
   );
 };
